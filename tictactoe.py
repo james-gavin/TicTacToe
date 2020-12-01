c152df7f0d9665578206f118c4aaec7e9597c85b
jmsgvn <owen@dubelyoo.com>
2020-12-01 08:38:34 -0600

    Instead of '-' inside the board we are now using numbers so players can easily deside where they want their next piece to be

--- tictactoe.py
+++ tictactoe.py
@@ -4,7 +4,7 @@ from subprocess import call
 def clear(): 
     _ = call('clear' if os.name =='posix' else 'cls') 
 
-board = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 game_running = True
 marking = 'X'
